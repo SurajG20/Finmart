7cdb73584301566a20ac68883335718828771f7c
Akshit Dhakad <116799118+akshitDhakad@users.noreply.github.com>
2023-11-09 18:45:32 +0530

    Akshit Updated 09-11-2023

--- public/js/userDetails.js
+++ public/js/userDetails.js
@@ -5,35 +5,66 @@ document.addEventListener('DOMContentLoaded', function () {
   var loanDetailBtn = document.getElementById('loanDetailBtn');
   var applicationDetailBtn = document.getElementById('applicationDetailBtn');
   var documentDetailBtn = document.getElementById('documentDetailBtn');
+  var notificationBtn = document.getElementById("notificationBtn");
 
   var personalDetail = document.getElementById('personalDetail');
   var loanDetail = document.getElementById('loanDetail');
   var applicationDetail = document.getElementById('applicationDetail');
   var documentDetail = document.getElementById('documentDetail');
+  var notificationContainer = document.getElementById("notificationContainer");
 
   // Show personalDetail by default and hide others
   showDiv(personalDetail);
-  hideOthers([loanDetail, applicationDetail, documentDetail]);
+  hideOthers([
+    loanDetail,
+    applicationDetail,
+    documentDetail,
+    notificationContainer,
+  ]);
 
   // Add click event listeners to the buttons
   personalDetailBtn.addEventListener('click', function () {
     showDiv(personalDetail);
-    hideOthers([loanDetail, applicationDetail, documentDetail]);
+    hideOthers([
+      loanDetail,
+      applicationDetail,
+      documentDetail,
+      notificationContainer,
+    ]);
   });
 
   loanDetailBtn.addEventListener('click', function () {
     showDiv(loanDetail);
-    hideOthers([personalDetail, applicationDetail, documentDetail]);
+    hideOthers([
+      personalDetail,
+      applicationDetail,
+      documentDetail,
+      notificationContainer,
+    ]);
   });
 
   applicationDetailBtn.addEventListener('click', function () {
     showDiv(applicationDetail);
-    hideOthers([personalDetail, loanDetail, documentDetail]);
+    hideOthers([
+      personalDetail,
+      loanDetail,
+      documentDetail,
+      notificationContainer,
+    ]);
   });
 
   documentDetailBtn.addEventListener('click', function () {
     showDiv(documentDetail);
-    hideOthers([personalDetail, loanDetail, applicationDetail]);
+    hideOthers([
+      personalDetail,
+      loanDetail,
+      applicationDetail,
+      notificationContainer,
+    ]);
+  });
+  notificationBtn.addEventListener("click", function () {
+    showDiv(notificationContainer);
+    hideOthers([personalDetail, loanDetail, applicationDetail, documentDetail]);
   });
 
   // Function to show a specific div
