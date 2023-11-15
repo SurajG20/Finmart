bd894bc4a0f48b93fc362ec1192f47ff30b557bf
Suraj Goswami <121960253+SurajG20@users.noreply.github.com>
2023-11-15 14:51:27 +0530

    few more changes

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,56 +1,73 @@
-document
-  .getElementById('calculateButton')
-  .addEventListener('click', function (e) {
-    e.preventDefault();
+const modal = document.getElementById('loanResultModal');
 
-    // Fetch values from the form
-    var loanType = document.getElementById('loandetails').value;
-    var loanAmount = parseFloat(document.getElementById('loan-amount').value);
-    var loanTime = parseInt(document.getElementById('loan-time').value);
-    var rateOfInterest = parseFloat(
-      document.getElementById('rate-interest').value
+// Get the button that opens the modal
+const calculateButton = document.getElementById('calculateButton');
+
+// Get the <span> element that closes the modal
+const closeModal = document.getElementById('closeModal');
+
+// When the user clicks the button, open the modal
+calculateButton.onclick = function () {
+  // Fetch values from the form
+  const loanType = document.getElementById('loandetails').value;
+  const loanAmount = parseFloat(document.getElementById('loan-amount').value);
+  const loanTime = parseInt(document.getElementById('loan-time').value);
+  const rateOfInterest = parseFloat(
+    document.getElementById('rate-interest').value
+  );
+
+  // Validate inputs
+  if (isNaN(loanAmount) || isNaN(loanTime) || isNaN(rateOfInterest)) {
+    alert(
+      'Please enter valid numeric values for loan amount, tenure, and interest rate.'
     );
+    return;
+  }
 
-    // Validate inputs
-    if (isNaN(loanAmount) || isNaN(loanTime) || isNaN(rateOfInterest)) {
-      alert(
-        'Please enter valid numeric values for loan amount, tenure, and interest rate.'
-      );
-      return;
-    }
+  // Convert annual interest rate to monthly and percentage to decimal
+  const monthlyInterestRate = rateOfInterest / 12 / 100;
 
-    // Convert annual interest rate to monthly and percentage to decimal
-    var monthlyInterestRate = rateOfInterest / 12 / 100;
+  // Convert loan tenure to months
+  const numberOfPayments = loanTime * 12;
 
-    // Convert loan tenure to months
-    var numberOfPayments = loanTime * 12;
+  // Calculate EMI using the formula
+  const emi =
+    (loanAmount *
+      monthlyInterestRate *
+      Math.pow(1 + monthlyInterestRate, numberOfPayments)) /
+    (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1);
 
-    // Calculate EMI using the formula
-    var emi =
-      (loanAmount *
-        monthlyInterestRate *
-        Math.pow(1 + monthlyInterestRate, numberOfPayments)) /
-      (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1);
+  // Display the result in the modal
+  document.getElementById('loanResult').innerHTML =
+    'Loan Type: ' +
+    loanType +
+    '<br>' +
+    'Loan Amount: ' +
+    loanAmount +
+    '<br>' +
+    'Tenure Period: ' +
+    loanTime +
+    ' years' +
+    '<br>' +
+    'Rate of Interest: ' +
+    rateOfInterest +
+    '%' +
+    '<br>' +
+    'EMI: &#8377; ' +
+    emi.toFixed(2);
 
-    // Display the result in the modal
-    document.getElementById('loanResult').innerHTML =
-      'Loan Type: ' +
-      loanType +
-      '<br>' +
-      'Loan Amount: ' +
-      loanAmount +
-      '<br>' +
-      'Tenure Period: ' +
-      loanTime +
-      ' years' +
-      '<br>' +
-      'Rate of Interest: ' +
-      rateOfInterest +
-      '%' +
-      '<br>' +
-      'EMI: &#8377; ' +
-      emi.toFixed(2);
+  // Show the modal
+  modal.style.display = 'flex';
+};
 
-    // Show the modal
-    document.getElementById('loanResultModal').style.display = 'flex';
-  });
+// When the user clicks on <span> (x), close the modal
+closeModal.onclick = function () {
+  modal.style.display = 'none';
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
