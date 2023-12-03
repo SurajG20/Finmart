65e12fc65ed86ea167d922cf2b18909eb0393399
Suraj Goswami <121960253+SurajG20@users.noreply.github.com>
2023-12-03 12:15:43 +0530

    Update image and profile details styling

--- models/CombinedDetails.js
+++ models/CombinedDetails.js
@@ -5,45 +5,54 @@ const documentSchema = new mongoose.Schema({
   filename: String,
 });
 
-const combinedDetailsSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-  },
-  loanDetails: {
-    type: {
-      'select-loan-type': String,
-      rateOfInterest: String,
-      tenureDuration: String,
-      loanAmount: String,
-      financingType: String,
-      bankService: String,
-      tenureUnit: String
-    },
-    required: true,
-  },
-  personalDetails: {
-    type: {
-      firstName: String,
-      lastName: String,
-      dateOfBirth: String,
-      MaritalStatus: String,
-      email: String,
-      mobileNumber: String,
-      presentAddress: String,
-      state: String,
-      city: String,
-      zipCode: String,
-    },
-    required: true,
-  },
-  documentUploads: [documentSchema],
-  ApplicationStatus: {
-    type: String,
-    enums: ['Pending', 'Approved', 'Rejected'],
-    default: 'Pending',
-  },
+documentSchema.virtual('thumbnail').get(function () {
+  return this.url.replace('/upload', '/upload/h_200,w_200');
 });
+
+const opts = { toJSON: { virtuals: true } };
+
+const combinedDetailsSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    loanDetails: {
+      type: {
+        'select-loan-type': String,
+        rateOfInterest: String,
+        tenureDuration: String,
+        loanAmount: String,
+        financingType: String,
+        bankService: String,
+        tenureUnit: String,
+      },
+      required: true,
+    },
+    personalDetails: {
+      type: {
+        firstName: String,
+        lastName: String,
+        dateOfBirth: String,
+        MaritalStatus: String,
+        email: String,
+        mobileNumber: String,
+        presentAddress: String,
+        state: String,
+        city: String,
+        zipCode: String,
+      },
+      required: true,
+    },
+    documentUploads: [documentSchema],
+    ApplicationStatus: {
+      type: String,
+      enums: ['Pending', 'Approved', 'Rejected'],
+      default: 'Pending',
+    },
+  },
+  opts
+);
 
 const CombinedDetails = mongoose.model(
   'CombinedDetails',
